3a825bb04b803ddfd4d788d7cfbdf30ee87521f4
joaoenderman123 <costa.oliveira18@escola.pr.gov.br>
2025-06-13 10:45:33 -0300

    Add files via upload

---
+++ agrinho/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Campo Cidade',
+    'qual região acontece mais desmatamentos?',
+    'A região que enfrenta o maior desmatamento no Brasil é o Cerrado'
+)
+
+criaCartao(
+    'Campo Cidade',
+    'quais as consequências do desmatamentos?',
+    'perda de biodiversidade, o aumento do aquecimento global, a erosão do solo, a contaminação da água e a perda de recursos hídricos.'
+)
+
+criaCartao(
+    'Campo Cidade',
+    'Em qual cidade ha o maior número de queimadas?',
+    'Em 2024, os municípios com o maior número de queimadas foram São Félix do Xingu (PA) e Altamira (PA), com 7,3 mil e 5,8 mil focos, respectivamente.'
+)
+
+criaCartao(
+    'Campo Cidade',
+    'quais os impactos que a queimada?',
+    'perda de biodiversidade e alterações no solo até impactos na saúde humana e no clima global.'
+)
+
+criaCartao(
+    'Campo Cidade',
+    'qual a diferença entre o campo e a cidade?',
+    'reside no tipo de atividades econômicas, densidade populacional e infraestrutura.'
+)
